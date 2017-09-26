312612f58effd251d20e57e057f8f8b9ac0d8478
Hileo Andersson <hileomsi@gmail.com>
2017-09-26 18:44:42 -0300

    fix InfiniteScrollPage

--- example/pages/InfiniteScrolPage.jsx
+++ example/pages/InfiniteScrolPage.jsx
@@ -2,36 +2,43 @@ import { h, Component } from 'preact';
 import { Page, Navbar, List, ListItem, ListGroup, BlockFooter, Block, PullToRefresh, InfiniteScroll } from '../../dist/preact-f7';
 
 export default class InfiniteScrollPage extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      items: []
+    }
+  }
+
+  componentWillMount() {
+    this.addItens();
+  }
+
+  addItens = () => {
+    const qtdItems = 15; 
+    const { items } = this.state;
+    let last = 0;
+    if(items.length) last = items[items.length - 1];
+
+    for(let i = 0; i < qtdItens; i++)
+      items.push(last + i);
+
+    this.setState({ items })
+  }
+
   render() {
     return (
       <Page>
-        <Navbar title='InfiniteScroll' />
-        <div className="block-title">Scroll bottom</div>
-        <div className="list simple-list">
-          <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-            <li>Item 4</li>
-            <li>Item 5</li>
-            <li>Item 6</li>
-            <li>Item 7</li>
-            <li>Item 8</li>
-            <li>Item 9</li>
-            <li>Item 10</li>
-            <li>Item 11</li>
-            <li>Item 12</li>
-            <li>Item 13</li>
-            <li>Item 14</li>
-            <li>Item 15</li>
-            <li>Item 16</li>
-            <li>Item 17</li>
-            <li>Item 18</li>
-            <li>Item 19</li>
-            <li>Item 20</li>
-          </ul>
-        </div>
-        <InfiniteScroll distance={2} onInfinite={() => console.log('OnInfinite')} />
+        <Navbar title='Infinite Scroll' />
+        <Block title='Scroll bottom' />
+        <List simple>
+          {this.state.items.map((number) => <ListItem>Item {number}</ListItem>)}
+        </List>
+        <InfiniteScroll distance={2} onInfinite={() => {
+          this.addItens();
+          console.log('OnInfinite');
+        }} />
       </Page>
     );
   }
