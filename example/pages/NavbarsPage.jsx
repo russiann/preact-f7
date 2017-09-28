9464168f9e4373343ab20aacb88ed705de50ed45
Rogan Melo <roganmelo@gmail.com>
2017-09-28 14:37:09 -0300

    fix(navbar): fix navbar page details

--- example/pages/NavbarsPage.jsx
+++ example/pages/NavbarsPage.jsx
@@ -3,14 +3,14 @@ import { Page, Navbar, List, ListItem, Block } from '../../dist/preact-f7';
 
 export default () => (
   <Page>
-    <Navbar title="Navbar" subtitle="Subtitle" right="Right" />
+    <Navbar title="Navbar" subtitle="Subtitle" right={<a class="link">Right</a>} />
     <Block>
       <p>Navbar is a fixed (with Fixed and Through layout types) area at the top of a screen that contains Page title and navigation elements.</p>
       <p>Navbar has 3 main parts: Left, Title and Right. Each part may contain any HTML content.</p>
     </Block>
    
     <List links>
-      <ListItem text="Hide Navbars On Scroll" link="/hide-navbars-on-scroll"/>
+      <ListItem text="Hide Navbar On Scroll" link="/hide-navbars-on-scroll"/>
     </List>
   </Page>
 );
