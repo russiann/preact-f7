f6c76c8397bde518bda9b5a44d151c9e777092c5
Russian Rebouças <russian.reboucas@gmail.com>
2017-11-11 20:10:45 -0300

    fix: inputs change events

--- src/components/Form/Inputs/InputText.jsx
+++ src/components/Form/Inputs/InputText.jsx
@@ -1,6 +1,8 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 import PropTypes from 'prop-types';
 import { createClassName } from 'create-classname';
+
+import { getInstance } from '../../../instance';
 
 const labelClass = createClassName('item-title item-label', [
   'floatLabel:item-floating-label'
@@ -10,28 +12,68 @@ const itemClass = createClassName('item-content item-input', [
   'info:item-input-with-info'
 ]);
 
-const InputText = ({ type, placeholder, label, media, children, value, info, ...props }) => (
-  <li className={itemClass({info})}>
+class InputText extends Component {
 
-    <If condition={media}>
-      <div className="item-media">{media}</div>
-    </If>
+  
+  componentDidMount() {
+    const Dom7 = getInstance().$;
+    Dom7(this.input).on('change input', (e) => {
+      if (this.props.onChange) {
+        this.props.onChange(e);
+      }
+    })
+  }
+  
+  render() {
+    const { type, placeholder, label, media, children, value, info, ...props } = this.props;
+    return (
+      <li className={itemClass({info})}>
 
-    <div className="item-inner">
-
-      <If condition={label} >
-        <div className={labelClass(props)}>{label}</div>
-      </If>
-      
-      <div className="item-input-wrap">
-        <input type={type} placeholder={placeholder} value={value} {...props} />
-        <span className="input-clear-button"></span>
-        <If condition={info}>
-          <div class="item-input-info">{info}</div>
+        <If condition={media}>
+          <div className="item-media">{media}</div>
         </If>
-      </div>
-    </div>
-  </li>
-);
+
+        <div className="item-inner">
+
+          <If condition={label} >
+            <div className={labelClass(props)}>{label}</div>
+          </If>
+          
+          <div className="item-input-wrap">
+            <input ref={(node) => this.input = node} type={type} placeholder={placeholder} value={value} {...props} />
+            <span className="input-clear-button"></span>
+            <If condition={info}>
+              <div class="item-input-info">{info}</div>
+            </If>
+          </div>
+        </div>
+      </li>
+    );
+  }
+}
+
+// const InputText = ({ type, placeholder, label, media, children, value, info, ...props }) => (
+//   <li className={itemClass({info})}>
+
+//     <If condition={media}>
+//       <div className="item-media">{media}</div>
+//     </If>
+
+//     <div className="item-inner">
+
+//       <If condition={label} >
+//         <div className={labelClass(props)}>{label}</div>
+//       </If>
+      
+//       <div className="item-input-wrap">
+//         <input ref="input" type={type} placeholder={placeholder} value={value} {...props} />
+//         <span className="input-clear-button"></span>
+//         <If condition={info}>
+//           <div class="item-input-info">{info}</div>
+//         </If>
+//       </div>
+//     </div>
+//   </li>
+// );
 
 export default InputText;
--- src/components/Form/Inputs/Range.jsx
+++ src/components/Form/Inputs/Range.jsx
@@ -1,28 +1,70 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 // import { createClassName } from 'create-classname';
 // const badgeClass = createClassName('badge', [{ name: 'color', className: ({ color }) => `color-${color}` }]);
 
-const InputText = ({ type, placeholder, label, media, ...props }) => (
-  <li className="item-content item-input">
+import { getInstance } from '../../../instance';
+
+class Range extends Component {
+  
+  componentDidMount() {
+    const Dom7 = getInstance().$;
+    Dom7(this.input).on('change input', (e) => {
+      if (this.props.onChange) {
+        this.props.onChange(e);
+      }
+    })
+  }
+
+  render() {
+    const { type, placeholder, label, media, onChange, ...props } = this.props;
+
+    return (
+      <li className="item-content item-input">
+      
+        <If condition={media}>
+          <div className="item-media">{media}</div>
+        </If>
     
-    <If condition={media}>
-      <div className="item-media">{media}</div>
-    </If>
-
-    <div className="item-inner">
-
-      <If condition={label} >
-        <div className="item-title item-label">{label}</div>
-      </If>
-
-      <div className="item-input-wrap">
-
-        <div className="range-slider range-slider-init" data-label="true">
-          <input type="range" {...props} />
+        <div className="item-inner">
+    
+          <If condition={label} >
+            <div className="item-title item-label">{label}</div>
+          </If>
+    
+          <div className="item-input-wrap">
+    
+            <div className="range-slider range-slider-init" data-label="true">
+              <input ref={(node) => this.input = node} type="range" {...props} />
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </li>
-);
+      </li>
+    )
+  }
 
-export default InputText;
+}
+
+// const InputText = ({ type, placeholder, label, media, ...props }) => (
+//   <li className="item-content item-input">
+    
+//     <If condition={media}>
+//       <div className="item-media">{media}</div>
+//     </If>
+
+//     <div className="item-inner">
+
+//       <If condition={label} >
+//         <div className="item-title item-label">{label}</div>
+//       </If>
+
+//       <div className="item-input-wrap">
+
+//         <div className="range-slider range-slider-init" data-label="true">
+//           <input type="range" {...props} />
+//         </div>
+//       </div>
+//     </div>
+//   </li>
+// );
+
+export default Range;
